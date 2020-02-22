680d4740efbc9afd35b562730f768ae5284cfecc
Mehmet.Yuce <mehmet.yuce@logosgt.com.tr>
2020-02-23 02:17:33 +0300

    challenge 2 without tests

--- device_db.py
+++ device_db.py
@@ -2,6 +2,9 @@ import json
 from pony.orm import *
 from pony.orm.dbapiprovider import StrConverter
 from enum import Enum
+import json
+
+PAGE = 'page'
 
 
 class EnumConverter(StrConverter):
@@ -42,15 +45,37 @@ db.generate_mapping(create_tables=True)
 
 
 @db_session
-def new_device_data(brand, model, os: OsType, osVersion):
-    new_data = DeviceData(brand=brand, model=model, os=os, osVersion=osVersion)
+def new_device_data(brand, model, os: OsType, os_version):
+    new_data = DeviceData(brand=brand, model=model, os=os, osVersion=os_version)
     commit()
-    return  str(new_data.id)
+    return str(new_data.id)
 
 
 @db_session
 def device_data_count():
     return count(p for p in DeviceData)
+
+
+def json_out(func):
+    def wrapper(**kwargs):
+        ret = func(**kwargs)
+        return json.dumps([p.to_dict() for p in ret], default=str)
+
+    return wrapper
+
+
+@db_session
+@json_out
+def query(**kwargs):
+    q = select(p for p in DeviceData)
+    for key, value in kwargs.items():
+        if value and key in DeviceData.__dict__:
+            q = q.filter(lambda x: value in kwargs[key])
+    if PAGE in kwargs:
+        page_num = kwargs[PAGE]
+        if page_num:
+            q = q.page(int(kwargs[PAGE]), 20)
+    return q
 
 
 cnt = device_data_count()
--- server.py
+++ server.py
@@ -1,27 +1,21 @@
-import random
-import string
-
 import cherrypy
+
 import device_db
 
 
-@cherrypy.expose
 class DeviceDataWebService(object):
-
     @cherrypy.tools.accept(media='application/json')
-    def POST(self, brand, model, os, osVersion):
+    @cherrypy.expose
+    def index(self, brand=None, model=None, os=None, osVersion=None):
+        if not brand:
+            return ""
         return device_db.new_device_data(brand, model, device_db.OsType[os], osVersion)
 
-
+    @cherrypy.expose(['devices'])
+    # @cherrypy.tools.json_out() # not working with Enum for now
+    def devices(self, brand=None, model=None, os=None, osVersion=None, page=None):
+        return device_db.query(brand=brand, model=model, os=device_db.OsType[os] if os else None, osVersion=osVersion, page=page)
 
 
 if __name__ == '__main__':
-    conf = {
-        '/': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.sessions.on': False,
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/javascript')],
-        }
-    }
-    cherrypy.quickstart(DeviceDataWebService(), '/', conf)
+    cherrypy.quickstart(DeviceDataWebService())
